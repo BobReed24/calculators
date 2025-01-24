00a74b3276a6383d3c7fa8cb3b8d1b28dfd1d82d
Hunkon <153524469+BobReed24@users.noreply.github.com>
2025-01-24 07:01:56 -0600

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,7 +1,8 @@
+import ast
 def main():
     while True:
         try:
-            res = eval(input(">: "))
+            res = float(ast.literal_eval(input(">: ")))
             print(res)
         except (ValueError, TypeError, NameError, SyntaxError) as e:
             print(f"Err: {e}")
